fc85f84bfdb20c490a0b53263aadc3520ee80cc6
aukro1408 <142673562+aukro1408@users.noreply.github.com>
2026-05-16 23:52:27 +0300

    Add USER_AGENT constant to server.js

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use((req, res, next) => {
 });
 
 const PORT = process.env.PORT || 8000;
+const USER_AGENT =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0 Safari/537.36';
 
 app.get('/', (req, res) => {
   res.json({
